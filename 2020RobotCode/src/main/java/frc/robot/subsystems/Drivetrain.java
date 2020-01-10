d6947d2d71a55e96a2a1bfd378fc61e529952cd5
sde998 <lilyx998@gmail.com>
2020-01-10 18:37:58 -0500

    moved create Drivetrain code out of constructor

--- 2020RobotCode/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2020RobotCode/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -20,38 +20,40 @@ public class Drivetrain extends SubsystemBase {
   /**
    * Creates a new Drivetrain.
    */
+  TalonFX frontLeftDrive = new TalonFX(Constants.FRONT_LEFT_DRIVE);
+  TalonFX frontLeftSteer = new TalonFX(Constants.FRONT_LEFT_STEER);
+  AnalogInput encoder1 = new AnalogInput(Constants.FRONT_LEFT_ENCODER);
+  SwerveModule module1 = new SwerveModule(frontLeftDrive, frontLeftSteer, encoder1, 1);
+
+
+  TalonFX rearLeftDrive = new TalonFX(Constants.REAR_LEFT_DRIVE);
+  TalonFX rearLeftSteer = new TalonFX(Constants.REAR_LEFT_STEER);
+  AnalogInput encoder2 = new AnalogInput(Constants.REAR_LEFT_ENCODER);
+  SwerveModule module2 = new SwerveModule(rearLeftDrive, rearLeftSteer, encoder2, 1);
+  
+
+  TalonFX frontRightDrive = new TalonFX(Constants.FRONT_RIGHT_DRIVE);
+  TalonFX frontRightSteer = new TalonFX(Constants.FRONT_RIGHT_STEER);
+  AnalogInput encoder3 = new AnalogInput(Constants.FRONT_RIGHT_ENCODER);
+  SwerveModule module3 = new SwerveModule(frontRightDrive, frontRightSteer, encoder3, 1);
+
+  TalonFX rearRightDrive = new TalonFX(Constants.REAR_RIGHT_DRIVE);
+  TalonFX rearRightSteer = new TalonFX(Constants.REAR_RIGHT_STEER);
+  AnalogInput encoder4 = new AnalogInput(Constants.REAR_RIGHT_ENCODER);
+  SwerveModule module4 = new SwerveModule(rearRightDrive, rearRightSteer, encoder4, 1);
+
+
+  PigeonIMU pigeon = new PigeonIMU(Constants.PIGEON);
+
+  SwerveControl swerve = new SwerveControl(module1, module2, module3, module4, pigeon);  
+
   public Drivetrain() {
-    TalonFX frontLeftDrive = new TalonFX(Constants.FRONT_LEFT_DRIVE);
-    TalonFX frontLeftSteer = new TalonFX(Constants.FRONT_LEFT_STEER);
-    AnalogInput encoder1 = new AnalogInput(Constants.FRONT_LEFT_ENCODER);
-    SwerveModule module1 = new SwerveModule(frontLeftDrive, frontLeftSteer, encoder1, 1);
-
-
-    TalonFX rearLeftDrive = new TalonFX(Constants.REAR_LEFT_DRIVE);
-    TalonFX rearLeftSteer = new TalonFX(Constants.REAR_LEFT_STEER);
-    AnalogInput encoder2 = new AnalogInput(Constants.REAR_LEFT_ENCODER);
-    SwerveModule module2 = new SwerveModule(rearLeftDrive, rearLeftSteer, encoder2, 1);
     
-
-    TalonFX frontRightDrive = new TalonFX(Constants.FRONT_RIGHT_DRIVE);
-    TalonFX frontRightSteer = new TalonFX(Constants.FRONT_RIGHT_STEER);
-    AnalogInput encoder3 = new AnalogInput(Constants.FRONT_RIGHT_ENCODER);
-    SwerveModule module3 = new SwerveModule(frontRightDrive, frontRightSteer, encoder3, 1);
-
-    TalonFX rearRightDrive = new TalonFX(Constants.REAR_RIGHT_DRIVE);
-    TalonFX rearRightSteer = new TalonFX(Constants.REAR_RIGHT_STEER);
-    AnalogInput encoder4 = new AnalogInput(Constants.REAR_RIGHT_ENCODER);
-    SwerveModule module4 = new SwerveModule(rearRightDrive, rearRightSteer, encoder4, 1);
-
-
-    PigeonIMU pigeon = new PigeonIMU(Constants.PIGEON);
-
-    SwerveControl swerve = new SwerveControl(module1, module2, module3, module4, pigeon);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    
+
   }
 }
