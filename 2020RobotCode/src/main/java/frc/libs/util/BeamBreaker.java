c3a56dedf5cd05ef6d5419036cbf0f60b9c4b530
Omegabots <31082313+Omegabots@users.noreply.github.com>
2020-01-11 11:24:15 -0500

    Fixed Errors

---
+++ 2020RobotCode/src/main/java/frc/libs/util/BeamBreaker.java
@@ -0,0 +1,13 @@
+package frc.libs.util;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class BeamBreaker extends DigitalInput{
+
+    public BeamBreaker(int channel) {
+        super(channel);
+    }
+
+    
+    
+}
--- 2020RobotCode/src/main/java/frc/robot/RobotContainer.java
+++ 2020RobotCode/src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,7 @@
 
 package frc.robot;
 
-import frc.libs.util.*;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
