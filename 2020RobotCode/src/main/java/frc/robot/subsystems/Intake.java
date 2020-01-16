bfee702f2a31eb0a38ffba302babd529a54ea885
arnavs-0 <31082313+Omegabots@users.noreply.github.com>
2020-01-16 18:53:18 -0500

    Changed Intake to have Active Floor Motors and Deleted Active Floor Subsystem

--- 2020RobotCode/src/main/java/frc/robot/subsystems/Intake.java
+++ 2020RobotCode/src/main/java/frc/robot/subsystems/Intake.java
@@ -24,6 +24,7 @@ public class Intake extends SubsystemBase {
   public void intake(double intakeSpeed){
     intake.set(TalonFXControlMode.PercentOutput, intakeSpeed);
   }
+
   public Intake() {
 
   }
