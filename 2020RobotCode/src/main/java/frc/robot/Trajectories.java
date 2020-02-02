2a367622415c8ae88c0742edf8e282e18afe8220
Nidhi Jaison <nidhijaison@gmail.com>
2020-02-02 15:37:29 -0500

    Creates new path when gettarget method is called

--- 2020RobotCode/src/main/java/frc/robot/Trajectories.java
+++ 2020RobotCode/src/main/java/frc/robot/Trajectories.java
@@ -12,9 +12,10 @@ import frc.libs.util.Limelight;
 public class Trajectories {
     static TrajectoryConfig config = new TrajectoryConfig(1, 0.5);
     
-    Pose2d[] targetPath = new Pose2d[] { new Pose2d(), new Pose2d(Limelight.getHorizontalOffset(), Limelight.distanceToTarget(), new Rotation2d(Limelight.getSkew())) };
+    Pose2d[] targetPath;
 
     public Trajectory getTargetTrajectory() {
-       return TrajectoryGenerator.generateTrajectory(Arrays.asList(targetPath), config);
+        targetPath = new Pose2d[] { new Pose2d(), new Pose2d(Limelight.getHorizontalOffset(), Limelight.distanceToTarget(), new Rotation2d(Limelight.getSkew())) };
+        return TrajectoryGenerator.generateTrajectory(Arrays.asList(targetPath), config);
     }
 }
