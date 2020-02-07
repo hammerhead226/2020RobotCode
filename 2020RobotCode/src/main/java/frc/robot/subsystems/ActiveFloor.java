25e6b0d7853ba276e0625de93d9f59c763df0389
AchyuthKashyap <32148791+AchyuthKashyap@users.noreply.github.com>
2020-02-06 19:37:30 -0500

    fixed command names and unused imports

--- 2020RobotCode/src/main/java/frc/robot/subsystems/ActiveFloor.java
+++ 2020RobotCode/src/main/java/frc/robot/subsystems/ActiveFloor.java
@@ -7,14 +7,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class ActiveFloor extends SubsystemBase {
   /**
