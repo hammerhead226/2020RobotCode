dac01ea2cb892313b21c6537aafb2020af3d16c4
Nidhi Jaison <nidhijaison@gmail.com>
2020-02-08 09:26:26 -0500

    fixed file names

---
+++ 2020RobotCode/src/main/java/frc/robot/commands/ToggleShooterHood.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Robot;
+
+public class ToggleShooterHood extends InstantCommand {
+  /**
+   * Creates a new toggleShooter.
+   */
+  
+  public ToggleShooterHood() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.pneumatics);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Robot.pneumatics.toggleShooter();
+  }
+}
