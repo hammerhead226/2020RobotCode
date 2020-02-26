3301c52497741435b9bfd260a371d93ba322636f
arnavs-0 <>
2020-02-26 08:49:24 -0500

    Added Interupt to Set Speed 0

--- 2020RobotCode/src/main/java/frc/robot/commands/ActiveFloorForward.java
+++ 2020RobotCode/src/main/java/frc/robot/commands/ActiveFloorForward.java
@@ -34,6 +34,7 @@ public class ActiveFloorForward extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    Robot.activeFloor.runActiveFloor(0);
   }
 
   // Returns true when the command should end.
