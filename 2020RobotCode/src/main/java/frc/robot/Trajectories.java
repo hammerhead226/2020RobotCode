7d40b5ca84ed946be03080144557dedc095f1ded
Nidhi Jaison <nidhijaison@gmail.com>
2020-02-28 19:22:18 -0500

    simp

--- 2020RobotCode/src/main/java/frc/robot/Trajectories.java
+++ 2020RobotCode/src/main/java/frc/robot/Trajectories.java
@@ -13,15 +13,19 @@ public class Trajectories {
         static TrajectoryConfig config = new TrajectoryConfig(Constants.MAX_DRIVE_VELOCITY,
                         Constants.MAX_DRIVE_ACCELERATION);
 
-        Pose2d[] targetPath;
+        static Pose2d[] targetPath;
 
-        public Trajectory getTargetTrajectory() {
+        public static Trajectory getTargetTrajectory() {
                 targetPath = new Pose2d[] { new Pose2d(), new Pose2d(
                                 Robot.drivetrain.currentPose2d.getTranslation().getX() - Constants.TARGET_X,
                                 Math.sqrt(Math.pow(Limelight.distanceToTarget(), 2)
                                                 - Math.pow(Robot.drivetrain.currentPose2d.getTranslation().getX()
                                                                 - Constants.TARGET_X, 2)),
                                 new Rotation2d(Limelight.getTargetRotation())) };
+                return TrajectoryGenerator.generateTrajectory(Arrays.asList(targetPath), config);
+        }
+        public static Trajectory simplePath(){
+                targetPath = new Pose2d[]{ new Pose2d(), new Pose2d(0, 5, new Rotation2d())};
                 return TrajectoryGenerator.generateTrajectory(Arrays.asList(targetPath), config);
         }
 }
